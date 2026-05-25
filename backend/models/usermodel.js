0ad08d76b71c11db01544a4188bcbb7ce792c5cf
Himanshu Pandey <himanshu805237@gmail.com>
2026-05-25 19:08:41 +0530

    code fixes db

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,21 +1,29 @@
-const db=require('../config/db');// db connect kiya import kiya 
+const db = require('../config/db');
 
-const findUserByEmail=(email,callback)=>{
-     const sql='SELECT * FROM users WHERE email = ?'; // placeholder
-    db.query(sql,[email],(err,results)=>{
-        if(err) return callback(err,null); // erroe check
-        callback(null,results[0])// pehla user wapas bheja 
+const findUserByEmail = (email, callback) => {
+    const sql = 'SELECT * FROM users WHERE email = ?';
+
+    db.query(sql, [email], (err, results) => {
+        if (err) {
+            console.log("DB ERROR (SELECT):", err);
+            return callback(err, null);
+        }
+
+        callback(null, results[0]);
     });
 };
 
-const createUser =(name,email,password,callback)=>{
-    const sql ='INSERT INTO users (name, email, password) VALUES (?, ?, ?)';//placeholder 
-    db.query(sql,[name,email,password],(err,results)=>{
-        if(err) return callback(err,null);//error check
-        callback(null,results); //success result wapas bhejo 
+const createUser = (name, email, password, callback) => {
+    const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
 
+    db.query(sql, [name, email, password], (err, results) => {
+        if (err) {
+            console.log("DB ERROR (INSERT):", err);  // 🔥 IMPORTANT
+            return callback(err, null);
+        }
+
+        callback(null, results);
     });
 };
 
-module.exports={findUserByEmail,createUser}; // export kiya dono function jo bnaya 
-
+module.exports = { findUserByEmail, createUser };
